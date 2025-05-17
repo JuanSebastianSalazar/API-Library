0eebdb2af9eab324887be946f2698d70a5f2ae77
JuanSebastianSalazar <131469508+JuanSebastianSalazar@users.noreply.github.com>
2025-05-17 18:15:08 -0500

    Ajustes del front

--- src/main/java/com/informaticonfig/spring_boot/security/SecurityClass.java
+++ src/main/java/com/informaticonfig/spring_boot/security/SecurityClass.java
@@ -1,16 +1,20 @@
 package com.informaticonfig.spring_boot.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityClass {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-        .csrf(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/autores/**").permitAll()
                         .requestMatchers("/api/libros/**").permitAll()
